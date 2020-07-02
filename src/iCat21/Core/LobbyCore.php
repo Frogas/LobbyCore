8081ad3767fe66fd94449d39256ca7788a017781
iCat21 <57547930+iCat21@users.noreply.github.com>
2020-07-02 13:48:31 +0000

    Create LobbyCore.php

---
+++ src/iCat21/Core/LobbyCore.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace iCat21\Core;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class LobbyCore extends PluginBase implements Listener{
+
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->info("Plugin Created Author by iCat21 Reza2175");
+        $this->core = new Config($this->getDataFolder()."Core.yml", Config::YAML);
+    }
+}
